5791848a615edc2b0aa6166dc72d57ba5b2e3b9d
Smrithi23 <smrithi.prakash23@gmail.com>
2023-12-07 20:27:58 -0500

    tournament update

--- tournament_4.py
+++ tournament_4.py
@@ -19,11 +19,9 @@ if __name__ == '__main__':
     # configuration 4
     # 210 runs
 
-    seeds = [1]
-    turns = [1440]
-    groups = [[1, 2], [1, 3], [1, 4], [1, 6],
-              [2, 3], [2, 4], [2, 6],
-              [3, 4], [3, 6], [4, 6]]
+    seeds = [1, 2]
+    turns = [10, 30, 60, 120, 180, 360]
+    groups = [[1, 5], [2, 5], [3, 5], [4, 5], [5, 6]]
     run = 1
     for seed in seeds:
         for turn in turns:
@@ -54,36 +52,36 @@ if __name__ == '__main__':
                     df.to_csv("config_4.csv", mode='a', header=False)
                 run += 1
 
-    seeds = [2]
-    turns = [10, 30, 60, 120, 180, 360, 1440]
-    groups = [[1, 2], [1, 3], [1, 4], [1, 6],
-              [2, 3], [2, 4], [2, 6],
-              [3, 4], [3, 6], [4, 6]]
-    for seed in seeds:
-        for turn in turns:
-            for group in groups:
-                try:
-                    print(run, seed, turn, group)
-                    args.teams=group
-                    args.seed=seed
-                    args.turns=turn
-                    args.scale=10
-                    args.gui="False"
-                    args.interval=1
-                    args.run=run
-                    wedding_game = WeddingGossip(args)
+    # seeds = [2]
+    # turns = [10, 30, 60, 120, 180, 360, 1440]
+    # groups = [[1, 2], [1, 3], [1, 4], [1, 6],
+    #           [2, 3], [2, 4], [2, 6],
+    #           [3, 4], [3, 6], [4, 6]]
+    # for seed in seeds:
+    #     for turn in turns:
+    #         for group in groups:
+    #             try:
+    #                 print(run, seed, turn, group)
+    #                 args.teams=group
+    #                 args.seed=seed
+    #                 args.turns=turn
+    #                 args.scale=10
+    #                 args.gui="False"
+    #                 args.interval=1
+    #                 args.run=run
+    #                 wedding_game = WeddingGossip(args)
 
-                    row = [run, seed, turns]
+    #                 row = [run, seed, turns]
 
-                    results = wedding_game.get_results()
-                    team_scores = list(results["team_scores"].values())
-                    group_score = results["group_score"]
-                    row1 = [run, seed, turn, group[0], team_scores[0], group_score]
-                    row2 = [run, seed, turn, group[1], team_scores[1], group_score]
-                    df = pd.DataFrame([row1, row2], columns=["Run", "Seed", "Turns", "Team Number", "Team Score", "Group Score"])
-                    df.to_csv("config_4.csv", mode='a', header=False)
-                except:
-                    row = [run, seed, turn, 'Error', 'Error', 'Error']
-                    df = pd.DataFrame([row], columns=["Run", "Seed", "Turns", "Team Number", "Team Score", "Group Score"])
-                    df.to_csv("config_4.csv", mode='a', header=False)
-                run += 1
+    #                 results = wedding_game.get_results()
+    #                 team_scores = list(results["team_scores"].values())
+    #                 group_score = results["group_score"]
+    #                 row1 = [run, seed, turn, group[0], team_scores[0], group_score]
+    #                 row2 = [run, seed, turn, group[1], team_scores[1], group_score]
+    #                 df = pd.DataFrame([row1, row2], columns=["Run", "Seed", "Turns", "Team Number", "Team Score", "Group Score"])
+    #                 df.to_csv("config_4.csv", mode='a', header=False)
+    #             except:
+    #                 row = [run, seed, turn, 'Error', 'Error', 'Error']
+    #                 df = pd.DataFrame([row], columns=["Run", "Seed", "Turns", "Team Number", "Team Score", "Group Score"])
+    #                 df.to_csv("config_4.csv", mode='a', header=False)
+    #             run += 1
